1640b5b8e833100c7159886d568eafb6c0f670e1
Xelpi <ankith3690@gmail.com>
2025-10-20 18:06:43 +0530

    Added SFML and made a window

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+
+int main() {
+
+    sf::Window window(sf::VideoMode({800, 600}), "Teto");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event -> is<sf::Event::Closed>()) window.close();
+        }
+    }
+
+    return 0;
+}
